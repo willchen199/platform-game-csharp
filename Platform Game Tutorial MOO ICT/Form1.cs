0f673a5a51a7193482a3c570e1e42e4fce659959
William Chen <williamexs@gmail.com>
2023-10-28 22:46:02 -0600

    zdv

---
+++ Platform Game Tutorial MOO ICT/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platform_Game_Tutorial_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+        // Variables for player movement and game state
+        bool goLeft, goRight, jumping, isGameOver;
+
+        int jumpSpeed;
+        int force;
+        int score = 0;
+        int playerSpeed = 7;
+
+        // Variables for platform and enemy movement speeds
+        int horizontalSpeed = 5;
+        int verticalSpeed = 3;
+        int enemyOneSpeed = 5;
+        int enemyTwoSpeed = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            // Update and display the player's score
+            txtScore.Text = "Score: " + score;
+
+            // Move the player based on input and jumping status
+            player.Top += jumpSpeed;
+            if (goLeft == true)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true)
+            {
+                player.Left += playerSpeed;
+            }
+
+            // Handle jumping and falling
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+            if (jumping == true)
+            {
+                jumpSpeed = -8;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 10;
+            }
+
+            // Collision detection and handling for platforms, coins, and enemies
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "platform")
+                    {
+                        // Handle collisions with platforms
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            force = 8;
+                            player.Top = x.Top - player.Height;
+
+                            // Handle horizontal platform movement
+                            if ((string)x.Name == "horizontalPlatform" && goLeft == false || (string)x.Name == "horizontalPlatform" && goRight == false)
+                            {
+                                player.Left -= horizontalSpeed;
+                            }
+                        }
+                        x.BringToFront();
+                    }
+
+                    if ((string)x.Tag == "coin")
+                    {
+                        // Handle collecting coins
+                        if (player.Bounds.IntersectsWith(x.Bounds) && x.Visible == true)
+                        {
+                            x.Visible = false;
+                            score++;
+                        }
+                    }
+
+                    if ((string)x.Tag == "enemy")
+                    {
+                        // Handle collisions with enemies (game over condition)
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            gameTimer.Stop();
+                            isGameOver = true;
+                            txtScore.Text = "Score: " + score + Environment.NewLine + "You were killed in your journey!!";
+                        }
+                    }
+                }
+            }
+
+            // Handle movement of horizontal platform
+            horizontalPlatform.Left -= horizontalSpeed;
+            if (horizontalPlatform.Left < 0 || horizontalPlatform.Left + horizontalPlatform.Width > this.ClientSize.Width)
+            {
+                horizontalSpeed = -horizontalSpeed;
+            }
+
+            // Handle vertical platform movement
+            verticalPlatform.Top += verticalSpeed;
+            if (verticalPlatform.Top < 195 || verticalPlatform.Top > 581)
+            {
+                verticalSpeed = -verticalSpeed;
+            }
+
+            // Handle movement of enemy one
+            enemyOne.Left -= enemyOneSpeed;
+            if (enemyOne.Left < pictureBox5.Left || enemyOne.Left + enemyOne.Width > pictureBox5.Left + pictureBox5.Width)
+            {
+                enemyOneSpeed = -enemyOneSpeed;
+            }
+
+            // Handle movement of enemy two
+            enemyTwo.Left += enemyTwoSpeed;
+            if (enemyTwo.Left < pictureBox2.Left || enemyTwo.Left + enemyTwo.Width > pictureBox2.Left + pictureBox2.Width)
+            {
+                enemyTwoSpeed = -enemyTwoSpeed;
+            }
+
+            // Check if the player fell off the screen (game over condition)
+            if (player.Top + player.Height > this.ClientSize.Height + 50)
+            {
+                gameTimer.Stop();
+                isGameOver = true;
+                txtScore.Text = "Score: " + score + Environment.NewLine + "You fell to your death!";
+            }
+
+            // Check if the player reached the door with the required score
+            if (player.Bounds.IntersectsWith(door.Bounds) && score == 26)
+            {
+                gameTimer.Stop();
+                isGameOver = true;
+                txtScore.Text = "Score: " + score + Environment.NewLine + "Your quest is complete!";
+            }
+            else
+            {
+                txtScore.Text = "Score: " + score + Environment.NewLine + "Collect all the coins";
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            // Handle key down events for player movement
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            // Handle key up events for player movement and game restart
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                // Restart the game
+                RestartGame();
+            }
+        }
+
+        private void RestartGame()
+        {
+            // Reset the game state and player position
+            jumping = false;
+            goLeft = false;
+            goRight = false;
+            isGameOver = false;
+            score = 0;
+            txtScore.Text = "Score: " + score;
+
+            // Make all coins visible again
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Visible == false)
+                {
+                    x.Visible = true;
+                }
+            }
+
+            // Reset player and enemy positions
+            player.Left = 72;
+            player.Top = 656;
+            enemyOne.Left = 471;
+            enemyTwo.Left = 360;
+            horizontalPlatform.Left = 275;
+            verticalPlatform.Top = 581;
+
+            // Restart the game timer
+            gameTimer.Start();
+        }
+    }
+}
